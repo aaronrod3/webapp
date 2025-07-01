9bcfb50df55cdc3246a927f759508f61d56561dd
aaron <aaronrod@me.com>
2025-07-01 13:22:45 -0400

    created api_url, start playing with GET requests

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-#test
-print("testing changes")
+import requests
+
+api_url = 'https://settings.svc.halowaypoint.com/settings/hipc/e2a0a7c6-6efe-42af-9283-c2ab73250c48'
+
+response = requests.get(url=api_url)
+print(response.text)
